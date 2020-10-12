90953460932d3ae25c21f8d60e45974acf3d5376
SharonFaith <sharonfaith15@gmail.com>
2020-10-12 10:02:59 +0300

    add create user function to run file

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3.6
 from user import User
 from credentials import Credentials
+
+def create_user(fname, lname, email, user_name, pword):
+
+    new_user = User(fname, lname, email, user_name, pword)
+    return new_user
