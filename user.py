aae4fac4559e3d11dd5cded576ac60b7eadee4b8
SharonFaith <sharonfaith15@gmail.com>
2020-10-12 09:23:46 +0300

    add method that defines user class

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    '''
+    Class that generates new instances of user
+    '''
+    user_list = []
+
+    def __init__(self, first_name, last_name, email, username, password):
+        '''
+        method that initiates instances of User class
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+        self.password = password
